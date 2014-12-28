751ec96e09d62fc54d8cda4447bd64fe7504a293
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-12-28 18:13:22 +0900

    follow PSR-2

--- tests/ClientFactoryTest.php
+++ tests/ClientFactoryTest.php
@@ -6,21 +6,21 @@ use OAuth\OAuth1\Service\Twitter;
 
 class ClientFactoryTest extends \PHPUnit_Framework_TestCase
 {
-	public function testCreateClient()
-	{
-		$client = (new ClientFactory)->createClient(
-			'Twitter',
-			'{CONSUMER_KEY}',
-			'{CONSUMER_SECRET}',
-			'{OAUTH_CALLBACK_URL}'
-		);
+    public function testCreateClient()
+    {
+        $client = (new ClientFactory)->createClient(
+            'Twitter',
+            '{CONSUMER_KEY}',
+            '{CONSUMER_SECRET}',
+            '{OAUTH_CALLBACK_URL}'
+        );
 
-		$this->assertInstanceOf(Twitter::class, $client);
-	}
+        $this->assertInstanceOf(Twitter::class, $client);
+    }
 
-	public function testCreateCallbackURL()
-	{
-		$url = (new ClientFactory)->createCallbackURL('/oauth/twitter/callback');
-		$this->assertEquals('http://example.com/oauth/twitter/callback', $url);
-	}
+    public function testCreateCallbackURL()
+    {
+        $url = (new ClientFactory)->createCallbackURL('/oauth/twitter/callback');
+        $this->assertEquals('http://example.com/oauth/twitter/callback', $url);
+    }
 }
--- tests/TwitterModuleTest.php
+++ tests/TwitterModuleTest.php
@@ -7,10 +7,10 @@ use Ray\Di\Injector;
 
 class TwitterModuleTest extends \PHPUnit_Framework_TestCase
 {
-	public function testTwitterModule()
-	{
-		$module = new TwitterModule('{CONSUMER_KEY}', '{CONSUMER_SECRET}', '{OAUTH_CALLBACK_PATH}');
-		$instance = (new Injector($module, $_ENV['TMP_DIR']))->getInstance(Twitter::class);
-		$this->assertInstanceOf(Twitter::class, $instance);
-	}
+    public function testTwitterModule()
+    {
+        $module = new TwitterModule('{CONSUMER_KEY}', '{CONSUMER_SECRET}', '{OAUTH_CALLBACK_PATH}');
+        $instance = (new Injector($module, $_ENV['TMP_DIR']))->getInstance(Twitter::class);
+        $this->assertInstanceOf(Twitter::class, $instance);
+    }
 }
