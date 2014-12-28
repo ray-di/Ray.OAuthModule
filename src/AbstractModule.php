a3339d95386782f6b1843083694b9e19d5110de8
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-12-28 17:52:18 +0900

    add file comment

--- src/AbstractModule.php
+++ src/AbstractModule.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * This file is part of the Ray.OAuthModule package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php MIT
+ */
 namespace Ray\OAuthModule;
 
 use OAuth\OAuth1\Service\ServiceInterface;
